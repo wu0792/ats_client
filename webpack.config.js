d5ed6d06ce33c7754c1cf7e55a7f1abec0411ae3
wu0792 <wu0792@163.com>
2018-08-09 23:02:13 +0800

    add theme

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
             { from: './src/html/devtools.html', to: './', force: true },
             { from: './src/html/panel.html', to: './', force: true },
             { from: './src/styles/panel.css', to: './', force: true },
+            { from: './src/styles/theme.css', to: './', force: true },
             { from: './src/images/icons.png', to: './', force: true },
         ], {})
     ],
